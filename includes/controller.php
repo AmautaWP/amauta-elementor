69b177c885efca312118d406c63854300ccf609c
Renzo Zamora <renzoster@gmail.com>
2020-09-29 07:41:57 -0500

    Updater Integrado

--- includes/controller.php
+++ includes/controller.php
@@ -19,11 +19,40 @@ class Controller
 
 	public function setup()
 	{
+		add_action( 'init', [ $this, 'setup_updater' ] );
+
 		$this->register_category();
 
 		// Dynamic Tags
 		new Dynamic_Tags\Has_Access;
 		new Dynamic_Tags\Course_Lesson;
+	}
+
+	public function setup_updater()
+	{
+		include_once AMAUTOR_PATH . '/includes/control/updater.php';
+
+		define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+		if ( is_admin() )
+		{
+			$config = [
+				'slug'               => plugin_basename( AMAUTOR_FILE ),
+				'proper_folder_name' => 'amauta-elementor',
+				'api_url'            => 'https://api.github.com/repos/AmautaWP/amauta-elementor',
+				'raw_url'            => 'https://raw.github.com/AmautaWP/amauta-elementor/master',
+				'github_url'         => 'https://github.com/AmautaWP/amauta-elementor',
+				'zip_url'            => 'https://github.com/AmautaWP/amauta-elementor/archive/master.zip',
+				'sslverify'          => true,
+				'requires'           => '5.0',
+				'tested'             => '5.3',
+				'readme'             => 'README.md',
+				'access_token'       => '',
+			];
+
+			new \WP_GitHub_Updater( $config );
+		}
+
 	}
 
 	public function register_category()
